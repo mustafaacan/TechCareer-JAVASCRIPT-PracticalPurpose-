2c0b6b359f80e6c0f61e47a06535e5155e602417
Mustafa Can <mustafaa.can.3535@gmail.com>
2026-01-17 02:15:54 +0300

    Locale Storage, DOM, EventListener

--- work14.js
+++ work14.js
@@ -1,11 +1,20 @@
-let userbtn = document.getElementById("btn1");
-let allbtn = document.getElementById("btn2");
 let user = document.getElementById("user");
 let all = document.getElementById("all");
+const userbtn = document.getElementById("btn1");
+const allbtn = document.getElementById("btn2");
+const nw = document.getElementById("btn3");
+const cl = document.getElementById("btn4");
 
-function locStoreADD() {
+function locStoreCLEAR() {
+  localStorage.clear();
+  all.innerText = "";
+  user.innerText = "";
+}
+
+function locStoreADDUSER() {
+  locStoreCLEAR();
   let name = prompt("Enter your name (will be saved on locale storage): ");
-  name ? name : "Username";
+  name = name === null || name === "" ? "Username" : name;
   localStorage.setItem("username", name);
   console.log(`${name} has added to locale storage`);
 
@@ -18,9 +27,24 @@ function locStoreADD() {
   return allItems;
 }
 
+function locStoreADD() {
+  let key = prompt("Enter the KEY :");
+  key = key === "" ? "EMPTY" : key;
+
+  if (key == null || key == "" || key == "EMPTY") {
+    alert(`${key} is not suitable for new locale starage name.`);
+  } else {
+    let value = prompt("Enter the VALUE :");
+    localStorage.setItem(String(key), value);
+    alert(`${key} and ${value} has added to Locale Storage`);
+  }
+  all.innerText = "";
+  user.innerText = "";
+}
+
 function localStorageREAD(item) {
   let result = localStorage.getItem(item);
-  return result ? result : None;
+  return result ? result : null;
 }
 
 function listener(event) {
@@ -32,12 +56,14 @@ function listener(event) {
       const key = localStorage.key(i);
       allItems[key] = localStorage.getItem(key);
     }
-    console.log(allItems);
+    all.innerText = JSON.stringify(allItems, null, 2);
   }
 }
 
-console.log("All the locale Storage elements: ", locStoreADD());
+console.log("All the locale Storage elements: ", locStoreADDUSER());
 console.log("The value on the locale Storage: ", localStorageREAD("username"));
 
 userbtn.addEventListener("click", listener);
 allbtn.addEventListener("click", listener);
+nw.addEventListener("click", locStoreADD);
+cl.addEventListener("click", locStoreCLEAR);
